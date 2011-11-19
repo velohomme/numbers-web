d8fc806830f000586f32950bec57933752f73b46
Jim Geppert <jimgeppert@gmail.com>
2011-11-18 19:19:34 -0600

    fixed some french words based on Andrew's edits

--- numbers/WebContent/js/text-intro-fr.js
+++ numbers/WebContent/js/text-intro-fr.js
@@ -10,9 +10,9 @@ InstructionsText =
 	"suggestionLink" : "Aidez-nous ameliorer cet outil!  Envoyez-nous une suggestion",
 	"ExcerciseName" : "Apprenez les <br/>nombres dans les langues étrangères",
 	"OptionsHeading" : "Les instructions:",
-	"SimpleExcerciseLink" : "<b>General.</b>  Cet outil vous permet de pratiquer les nombres hors contexte.",
-	"MoneyExcerciseLink" : "<b>Au magasin.</b> Avec cet outil vous pouvez ecouter les nombres dans la contexte le plus courant: au magasin.", 
-	"ClockExcerciseLink" : "<b>Quelle heure est-il?</b> Cet outil vous donne l'opportunité d'ecouter les nombres dans un context vraiment courant: les heures.",
+	"SimpleExcerciseLink" : "<b>En général.</b>  Cet outil vous permet de pratiquer les nombres hors contexte.",
+	"MoneyExcerciseLink" : "<b>Au magasin.</b> Avec cet outil vous pouvez écouter les nombres dans un contexte quotidien: au magasin.", 
+	"ClockExcerciseLink" : "<b>Quelle heure est-il?</b> Cet outil vous donne l'opportunité d'écouter les nombres dans un contexte vraiment quotidien: l'heure.",
 	"OverviewText" : "Utiliser ces outils pour practiquer les nombres en ",
 	'otherLanguagesTitle' :'Autres Langues:'
 };
